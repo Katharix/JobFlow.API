1c046648a4dee30edee61e0475e21e0427e24586
bllr777 <jerry.daniel.phillips@gmail.com>
2025-03-03 00:02:05 -0500

    Organization Service

---
+++ JobFlow.Business/Services/ServiceInterfaces/IOrganizationService.cs
@@ -0,0 +1,17 @@
+﻿using JobFlow.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobFlow.Business.Services.ServiceInterfaces
+{
+    public interface IOrganizationService
+    {
+        Task<Result<Organization>> GetOrganiztionById(Guid OrgId);
+        Task<Result<IEnumerable<Organization>>> GetAllOrganizations();
+        Task<Result<Organization>> UpsertOrganizatiom(Organization model);
+        Task<Result> DeleteOrganization(Guid organizationId);
+    }
+}
