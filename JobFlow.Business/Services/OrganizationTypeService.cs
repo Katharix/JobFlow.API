00c026b33518b989edb74c2adfd1a2c427f006ff
bllr777 <jerry.daniel.phillips@gmail.com>
2025-03-10 00:13:22 -0400

    Update projects and add initial org type service

---
+++ JobFlow.Business/Services/OrganizationTypeService.cs
@@ -0,0 +1,52 @@
+﻿using JobFlow.Business.Services.ServiceInterfaces;
+using JobFlow.Domain.Models;
+using JobFlow.Infrastructure.Persistence;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobFlow.Business.Services
+{
+    public class OrganizationTypeService : IOrganizationTypeService
+    {
+        private readonly ILogger<OrganizationTypeService> logger;
+        private readonly IUnitOfWork unitOfWork;
+        public OrganizationTypeService(ILogger<OrganizationTypeService> logger, IUnitOfWork unitOfWork)
+        {
+            this.logger = logger;
+            this.unitOfWork = unitOfWork;
+        }
+        public Task DeleteMultipleOrganizationTypes(IEnumerable<OrganizationType> idList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteOrganizationType(Guid organizationTypeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrganizationType> GetTypeById(Guid organizationTypeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<OrganizationType>> GetTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<OrganizationType>> UpsertOrganizationList(IEnumerable<OrganizationType> modelList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrganizationType> UpsertOrganizationType(OrganizationType model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JobFlow.Business/Services/ServiceInterfaces/IOrganizationTypeService.cs
@@ -0,0 +1,19 @@
+﻿using JobFlow.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobFlow.Business.Services.ServiceInterfaces
+{
+    interface IOrganizationTypeService
+    {
+        Task<IEnumerable<OrganizationType>> GetTypes();
+        Task<OrganizationType> GetTypeById(Guid organizationTypeId);
+        Task<OrganizationType> UpsertOrganizationType(OrganizationType model);
+        Task<IEnumerable<OrganizationType>> UpsertOrganizationList(IEnumerable<OrganizationType> modelList);
+        Task DeleteOrganizationType(Guid organizationTypeId);
+        Task DeleteMultipleOrganizationTypes(IEnumerable<OrganizationType> idList);
+    }
+}
